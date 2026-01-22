b4f6170ebe2f02065a676f307c1ee5aa70770567
Nguyen Pham Hoang Yen <nguyenphamhoangyen1101@gmail.com>
2026-01-22 19:03:06 +0700

    23540081-Add models and structures

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/database.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());

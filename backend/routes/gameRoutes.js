e2bedffcb8c8264cb4d34106a378e976f10f9b6a
An <Ans>
2026-03-09 19:23:39 +0700

    23540051 - search bar and cart's route

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -49,7 +49,7 @@ router.get("/", async (req, res) => {
     }
 
     if (search) {
-      query.$text = { $search: search };
+      query.name = { $regex: search, $options: "i" };
     }
 
     // Calculate pagination

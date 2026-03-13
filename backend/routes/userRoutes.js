049c0d87193fd34f6a22aa7fd8f5aa0b190df4dc
shaunsleepingsheep <nguyenhohoangson1005@gmail.com>
2026-03-13 17:19:05 +0700

    test

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,19 +2,18 @@ import express from "express";
 import User from "../models/user.js";
 import { protect } from "../middleware/authMiddleware.js";
 
-
-
 const router = express.Router();
+
 
 router.get("/profile", protect, async (req, res) => {
   try {
     const user = await User.findById(req.user.id);
-    res.json({ user });
+    res.json({ user }); // ← thêm wrapper
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 });
-
+// UPDATE profile
 router.put("/profile", protect, async (req, res) => {
   try {
     const { username, country, bio, avatar } = req.body;
@@ -23,33 +22,46 @@ router.put("/profile", protect, async (req, res) => {
       { username, country, bio, avatar },
       { new: true }
     );
-    res.json({ user });
+    res.json({ user }); // ← thêm wrapper
   } catch (error) {
     res.status(500).json({ message: "Update failed" });
   }
 });
-
+// CHANGE PASSWORD
 router.put("/change-password", protect, async (req, res) => {
 
   try {
+
     const { oldPassword, newPassword } = req.body;
+
     const user = await User.findById(req.user.id).select("+password");
+
     const isMatch = await user.comparePassword(oldPassword);
+
     if (!isMatch) {
+
       return res.status(400).json({
         message: "Old password incorrect"
       });
+
     }
+
     user.password = newPassword;
+
     await user.save();
+
     res.json({
       message: "Password updated"
     });
+
   } catch (error) {
+
     res.status(500).json({
       message: "Server error"
     });
+
   }
+
 });
 
 export default router;
